299690622ecea89c0635034c39fe202dea7e6764
YuFish-W <1219471845@qq.com>
2022-09-05 18:52:15 +0800

    第一次

---
+++ src/main/java/com/yufish/Controller/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.yufish.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@ResponseBody
+public class HelloWorldController {
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+}
